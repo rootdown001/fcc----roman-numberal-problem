e2244835de3ef60eb09c28d338d536dc968d9088
Lance Anderson <lance.a.585@gmail.com>
2022-10-09 16:55:38 -0700

    ADD
    Add variables for roman numeral segments

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 function convertToRoman(num) {
+    
+    // declare answer variable
     let answer = "";
-    let fourDig, threeDig, twoDig, oneDig = 0;
+    // declare variables to hold thousands, hundreds, tens and ones
+    let fourDig = 0; 
+    let threeDig = 0;
+    let twoDig = 0;
+    let oneDig = 0;
+    // declare variables to hold Roman numerals for thousands, hundreds, tens and ones
+    let fourRom = "";
+    let threeRom = "";
+    let twoRom = "";
+    let oneRom = "";
 
-
-
+    // console.log variables for digits
     console.log("fourDig = " + fourDig);
     console.log("threeDig = " + threeDig);
     console.log("twoDig = " + twoDig);
     console.log("oneDig = " + oneDig);
+    // console.log variables for roman
+    console.log("fourRom = " + fourRom);
+    console.log("threeRom = " + threeRom);
+    console.log("twoRom = " + twoRom);
+    console.log("oneRom = " + oneRom);
+
+    
     return answer;
    }
    
    convertToRoman(36);
+
