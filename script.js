a3fc2919e81ff8ae8fa01a89e8d3d44b44ca8304
RootDownCrypto.nft <37199096+rootdown001@users.noreply.github.com>
2022-10-09 16:28:32 -0700

    ADD
    Started script.js for this problem

---
+++ script.js
@@ -0,0 +1,14 @@
+function convertToRoman(num) {
+    let answer = "";
+    let fourDig, threeDig, twoDig, oneDig = 0;
+
+
+
+    console.log("fourDig = " + fourDig);
+    console.log("threeDig = " + threeDig);
+    console.log("twoDig = " + twoDig);
+    console.log("oneDig = " + oneDig);
+    return answer;
+   }
+   
+   convertToRoman(36);
