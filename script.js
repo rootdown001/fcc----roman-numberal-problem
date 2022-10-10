4d40097304b86f3c123599f7caa3b1d52a841c7f
Lance Anderson <lance.a.585@gmail.com>
2022-10-09 17:31:19 -0700

    Add
    Add logic to turm number into 4 variables for thousands, hundreds, etc

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
+const { ADDRCONFIG } = require("dns");
+
 function convertToRoman(num) {
     
     // declare answer variable
     let answer = "";
+    
     // declare variables to hold thousands, hundreds, tens and ones
     let fourDig = 0; 
     let threeDig = 0;
     let twoDig = 0;
     let oneDig = 0;
+    
     // declare variables to hold Roman numerals for thousands, hundreds, tens and ones
     let fourRom = "";
     let threeRom = "";
     let twoRom = "";
     let oneRom = "";
 
+    // array for key / value pairs
     let arr = [
         ["M", 1000], 
         ["CM", 900],
@@ -28,6 +33,12 @@ function convertToRoman(num) {
         ["IV",   4],
         ["I",    1]
     ]
+
+    // turn num into 4 diff variables for thousands, hundreds, etc
+    fourDig = (Math.floor(num/1000))*1000;
+    threeDig = (Math.floor((num - fourDig)/100))*100;
+    twoDig = (Math.floor((num - fourDig - threeDig)/10))*10;
+    oneDig = (Math.floor((num - fourDig - threeDig - twoDig)));
 
 
     // console.log variables for digits
@@ -49,7 +60,8 @@ function convertToRoman(num) {
     return answer;
    }
    
-   convertToRoman(36);
+   convertToRoman(3232);
 
 //commit text
+
 
